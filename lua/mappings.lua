bd5072e740a201f6e4543f08f96d2a853a1e4025
BigPigMoon <matvey.novikov02@gmail.com>
2024-10-16 11:54:51 +0700

    add tab switch mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,4 +6,11 @@ local map = vim.keymap.set
 
 map("i", "jk", "<ESC>")
 
+map("n", "H", function()
+  require("nvchad.tabufline").prev()
+end)
+
+map("n", "L", function()
+  require("nvchad.tabufline").next()
+end)
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
