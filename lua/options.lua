b83f219ce7c494aa473da16643450537d53d5ca4
BigPigMoon <matvey.novikov02@gmail.com>
2024-10-16 11:55:05 +0700

    add usefull vim opt settings

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,21 @@ require "nvchad.options"
 
 -- add yours here!
 
+local o = vim.opt
+
+o.rnu = true
+o.scrolloff = 7
+o.swapfile = false
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup "highlight_yank",
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
